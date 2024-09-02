4933edb2909f9cd39be12bd8abd6d2d48f72a44a
amb2007 <agustinmancinibobadilla@gmail.com>
2024-09-02 16:57:11 -0300

    no-me-funcionaba

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import './App.css';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 function App() {
-
+  const [data, setData] = useState([])
   const [term, setTerm] = useState("")
-
+  const firstAdv = useEffect(() => {
+    axios.get('https://api.adviceslip.com/advice')
+         .then((res) => setData(res.data))
+         .catch((error) => console.log(error.message))
+},[]);
   const handleTermChange = (event) => setTerm(event.target.value);
-
+  const handleClick =() => {
+      setData(axios.get('https://api.adviceslip.com/advice')
+         .then((res) => setData(res.data))
+         .catch((error) => console.log(error.message)))}
+  //fede acordate de que a mi no me corria por temas de la compu
   return (
     <main>
 
       <h1>Evaluación React - Requests</h1>
       <h1>Consejos de vida</h1>
-
+      <h2>{firstAdv}</h2>
       <div>
         <h2>Obtener un consejo aleatorio</h2>
-        <button>Obtener</button>
-        <p className="result-box"></p>
+        <button onClick={handleClick}>Obtener</button>
+        <p className="result-box">{data}</p>
       </div>
 
       <div>
         <h2>Buscar un consejo</h2>
         <input type="text" onChange={handleTermChange} />
-        <button>Enviar</button>
-        <h3>Resultados de búsqueda:</h3>
+        <button onClick={handleClick}>Conseguir</button>
+        <h1>{advice}</h1>
+        <h3>Resultados de búsqueda: </h3>
         <p className="result-box"></p>
       </div>
 
